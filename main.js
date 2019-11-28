2fd629b331fdc84771b7024bcebe481f2600c89d
Ivan <ivan.nikitich.90@inbox.ru>
2019-11-28 11:26:50 +0400

    Initial commit 2

---
+++ main.js
@@ -0,0 +1,65 @@
+const {app, BrowserWindow, Menu} = require("electron");
+const path = require("path");
+const url = require("url");
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+  // mainWindow.setIcon(path.join(__dirname, '/src/assets/images/png/logo.png'));
+
+  // load the dist folder from Angular
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, `/dist/index.html`),
+      protocol: "file:",
+      slashes: true
+    })
+  ).then(r => r);
+
+  // The following is optional and will open the DevTools:
+  mainWindow.webContents.openDevTools();
+
+  // Building menu from given template
+  const mainMenuTemplate = [
+    {
+      label: 'File',
+      submenu: [
+        {role: 'reload'},
+        {type: 'separator'},
+        {
+          label: 'Exit',
+          click() {
+            app.quit();
+          }
+        }
+      ]
+    }
+  ];
+
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+
+  // Insert menu
+  Menu.setApplicationMenu(mainMenu);
+
+  // Handles closing the main window
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+}
+
+app.on("ready", createWindow);
+
+// on macOS, closing the window doesn't quit the app
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+// initialize the app's main window
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {LoginComponent} from './auth/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent ,
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
